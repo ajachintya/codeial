fe03e7e9514f599fd19ff6a60feb0660da7fe1a7
ajachintya <40269281+ajachintya@users.noreply.github.com>
2019-09-22 23:40:22 +0530

    displayed post and related user

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,13 +1,26 @@
 //A group of actions is called as controller
-
+const Post=require('../models/post');
 //an exported function home
 module.exports.home=function(req,res){
     //'home' is for home.ejs 
     //rendering home.ejs
-    console.log(req.cookies);
-    res.cookie('user_id',111);
-    return res.render('home',{
-        title:"home"
+    // console.log(req.cookies);
+    // res.cookie('user_id',111);
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title:"Codeial | home",
+    //         posts:posts
+             
+    // })
+    
+    // });
+
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:"Codeial | home",
+            posts:posts
+             
+    });
     });
 }
 
