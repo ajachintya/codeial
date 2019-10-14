f2c89985c94194f2d07aa0f917ae9f4f1fdb8285
ajachintya <40269281+ajachintya@users.noreply.github.com>
2019-10-14 23:16:36 +0530

    deleting a post

--- routes/posts.js
+++ routes/posts.js
@@ -7,5 +7,6 @@ const router=express.Router();
 const postsControllers = require('../controller/posts_controller');
 
 router.post('/create',passport.checkAuthentication,postsControllers.create);
+router.get('/destory/:id',passport.checkAuthentication,postsControllers.destory);
 
 module.exports = router;
