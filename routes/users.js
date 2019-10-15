1a4c4d9aca711d8350253ba68d7eecc11e546b1d
ajachintya <40269281+ajachintya@users.noreply.github.com>
2019-10-15 21:10:34 +0530

    updating a user's profile

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ const passport=require('passport');
 const usersController=require('../controller/users_controller');
 //for prifile
 router.get('/profile/:id',passport.checkAuthentication,usersController.profile);
-
+router.post('/update/:id',passport.checkAuthentication,usersController.update);
 router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);
 router.post('/create',usersController.create);
